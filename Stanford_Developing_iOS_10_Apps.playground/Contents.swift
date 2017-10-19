45bdcf4bf09ee75bab0c07018ab039c53b3bca45
Kevin Tran <kevin2005tran@gmail.com>
2017-10-18 21:12:29 -0700

    first commit

---
+++ Stanford_Developing_iOS_10_Apps.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+//Notes from Stanford's Developing iOS 9 Apps course
+
+
+
+//What's an API? Functions & methods used to make an iOS app work
+//What operating system does an Iphone run? UNIX
+//What makes up a computer? CPU "Brain", RAM "Memory", Hard Disk "Instructional/ Data"
+//How does a computer work? CPU reads information from the hard disk and brings it to the RAM, this is done b/c before the CPU can do any kind of processing, calculations, computations it has to be brought to the RAM. After the CPU has modified the data it is then stored back onto the hard disk
+//What is an operating system? OS/Controls all the parts of a computer, hardware and software. Allocates resources and schedules tasks
+//What are the main components of iOS (Top - user to Bottom - hardware)? Cocoa Touch, Media, Core Services, Core OS
+
+
