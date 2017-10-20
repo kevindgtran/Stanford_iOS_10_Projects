3c5e1ece02a09835b2ebaca786669a7055cf1d4a
Kevin Tran <kevin2005tran@gmail.com>
2017-10-19 21:01:34 -0700

    starting lesson 2 notes

--- Stanford_Developing_iOS_10_Apps.playground/Contents.swift
+++ Stanford_Developing_iOS_10_Apps.playground/Contents.swift
@@ -14,4 +14,10 @@ import PlaygroundSupport
 //What is an operating system? OS/Controls all the parts of a computer, hardware and software. Allocates resources and schedules tasks
 //What are the main components of iOS (Top - user to Bottom - hardware)? Cocoa Touch, Media, Core Services, Core OS
 //What are the 5 states of a UIButton? The five states that define their appearance: default, highlighted, focused, selected, and disabled
+//What is MVC? Model(data/brains), View(minions/user interface objects. ie UILabels), Controller(communicator between the model and view. ie IBOutlets)
+//How does the view communicate back to the controller? The view "minions" send back an action to the target
+//View objects can synchronously communicate with the controller with keywords like (will, should, did, etc.) This gives more options for control on the view objects. These "special" methods are executed with Delegates. Delegates are special variables of a protocol type. So the controller signs up to be the delegate and listen for those special methods from the view. Delegates are all about communication between views and controllers
+//Views do not own the data they display. What does that mean? The data displayed on the view should not be stored in the view objects. ie the UITableView object (view) should not own all the possible data put into it (potentially thousands of songs). Instead we use another special protocol variable called DataSource. Datasource has other sets of methods to find and retrieve data from the model and display them on the view object (ie a table view with millions of songs). NOTE: Controllers are always the Datasource not the model
+//How does the model communicate changes to the data to the controller? The model broadcasts changes to the controller (like a radio station). Then the controller checks what changes were made.
+//Where can we have the MVC architecture in an app? The MVC architecture can be the whole app, on each page, on sub sections of a page, etc.
 
